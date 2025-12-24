30882e029fb1e5e66cebb1c974a72023564d4464
Rohini Gaikwad <rohinigaikwad7057@gmail.com>
2025-12-25 00:26:19 +0400

    Fix responsive navbar, hero text wrapping, and project card spacing

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, HostListener } from '@angular/core';
-import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { Component, AfterViewInit } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -9,33 +9,30 @@ import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent  {
+
   activeSection = 'home-section';
-
-  scrollTo(id: string) {
-    document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
-  }
-
-  @HostListener('window:scroll', [])
-  onScroll() {
-    const sections = [
-      'home-section',
-      'about-section',
-      'resume-section',
-      'project-section',
-      'contact-section'
-    ];
-
-    for (let section of sections) {
-      const el = document.getElementById(section);
-      if (!el) continue;
-
-      const rect = el.getBoundingClientRect();
-      if (rect.top <= 120 && rect.bottom >= 120) {
-        this.activeSection = section;
-        break;
-      }
-    }
+  closeMenu() {
+  const nav = document.getElementById('ftco-nav');
+  if (nav && nav.classList.contains('show')) {
+    nav.classList.remove('show');
   }
 }
 
+
+  // ngAfterViewInit() {
+  //   const sections = document.querySelectorAll('section[id]');
+
+  //   const observer = new IntersectionObserver(entries => {
+  //     entries.forEach(entry => {
+  //       if (entry.isIntersecting) {
+  //         this.activeSection = entry.target.id;
+  //       }
+  //     });
+  //   }, {
+  //     rootMargin: '-45% 0px -45% 0px'
+  //   });
+
+  //   sections.forEach(section => observer.observe(section));
+  // }
+}
